88bd6e534c9baee50396ad4c53b5b8d90f44cb2c
Carl Holmberg <holmberg.carl.j@gmail.com>
2021-04-16 21:32:12 +0100

    some starting stuff

---
+++ Assets/Scripts/AbstractFactoryObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractFactoryObject : MonoBehaviour
+{
+    public string name;
+    public AbstractFactoryObject input;
+    public AbstractFactoryObject output;
+
+
+}
