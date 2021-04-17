f833b3a72d5acc6f77c7558e93ebefa557f94dfe
Dino Pasalic <dinopasalic1999@gmail.com>
2021-04-17 19:37:12 +0200

    sprites

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,9 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     public float movementSpeed;
     public float smoothing;
+    public int ticksBetweenAnimation = 5;
+    public enum AnimationDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        Idle
+    }
+    public Sprite[] spriteup;
+    public Sprite[] spritedown;
+    public Sprite[] spriteleft;
+    public Sprite[] spriteright;
+    public SpriteRenderer spriterenderer;
 
     private Rigidbody2D rigidBody;
     private Vector3 velocity = Vector3.zero;
+    private AnimationDirection animationDirection = AnimationDirection.Down;
+    private int animationIndex = 0;
+    private int framecounter = 0;
+
 
     private void Awake()
     {
@@ -30,5 +48,112 @@ public class PlayerController : MonoBehaviour
         targetVelocity.Normalize();
         targetVelocity *= movementSpeed;
         rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, smoothing);
+    }
+
+    private void FixedUpdate()
+    {
+        CheckDirection();
+        Debug.Log(animationDirection);
+        if (animationDirection == AnimationDirection.Up)
+        {
+            if (ticksBetweenAnimation <= framecounter)
+            {
+                framecounter = 0;
+                animationIndex++;
+                if (animationIndex >= spriteup.Length)
+                {
+                    animationIndex = 0;
+                }
+            }
+            else
+            {
+                framecounter++;
+            }
+            spriterenderer.sprite = spriteup[animationIndex];
+        }
+        else if (animationDirection == AnimationDirection.Down)
+        {
+            if (ticksBetweenAnimation <= framecounter)
+            {
+                framecounter = 0;
+                animationIndex++;
+                if (animationIndex >= spritedown.Length)
+                {
+                    animationIndex = 0;
+                }
+            }
+            else
+            {
+                framecounter++;
+            }
+            spriterenderer.sprite = spritedown[animationIndex];
+        }
+        else if (animationDirection == AnimationDirection.Left)
+        {
+            if (ticksBetweenAnimation <= framecounter)
+            {
+                framecounter = 0;
+                animationIndex++;
+                if (animationIndex >= spriteup.Length)
+                {
+                    animationIndex = 0;
+                }
+            }
+            else
+            {
+                framecounter++;
+            }
+            spriterenderer.sprite = spriteleft[animationIndex];
+        }
+        else if (animationDirection == AnimationDirection.Right)
+        {
+            if (ticksBetweenAnimation <= framecounter)
+            {
+                framecounter = 0;
+                animationIndex++;
+                if (animationIndex >= spriteright.Length)
+                {
+                    animationIndex = 0;
+                }
+            }
+            else
+            {
+                framecounter++;
+            }
+            spriterenderer.sprite = spriteright[animationIndex];
+        }
+    }
+    void CheckDirection()
+    {
+        if (Mathf.Abs(rigidBody.velocity.x) > Mathf.Abs(rigidBody.velocity.y))
+        {
+            if (Mathf.Abs(rigidBody.velocity.x) < 0.01f)
+            {
+                animationDirection = AnimationDirection.Idle;
+            }
+            else if (rigidBody.velocity.x > 0.0f)
+            {
+                animationDirection = AnimationDirection.Right;
+            }
+            else
+            {
+                animationDirection = AnimationDirection.Left;
+            }
+        }
+        else if (Mathf.Abs(rigidBody.velocity.y) > Mathf.Abs(rigidBody.velocity.x))
+        {
+            if (Mathf.Abs(rigidBody.velocity.y) < 0.01f)
+            {
+                animationDirection = AnimationDirection.Idle;
+            }
+            else if (rigidBody.velocity.y > 0.0f)
+            {
+                animationDirection = AnimationDirection.Up;
+            }
+            else
+            {
+                animationDirection = AnimationDirection.Down;
+            }
+        }
     }
 }
