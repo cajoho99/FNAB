a3c4cf232a2e6314f09beb9861f1a29d87c1c85c
Carl Holmberg <holmberg.carl.j@gmail.com>
2021-04-18 09:35:57 +0100

    Simple customer

--- Assets/Scripts/GenericFood.cs
+++ Assets/Scripts/GenericFood.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public abstract class GenericFood : MonoBehaviour
 {
+    public Ingredient[] ingredients;
+    public Consistency consistency;
+    public FoodTexture foodTexture;
     public abstract void cook();
+}
+
+
+public enum Consistency
+{
+    Standard, Pureed
+}
+
+public enum FoodTexture
+{
+    Rough, Bland, 
+}
+
+public enum Ingredient 
+{
+    Fish,
+    Potato,
+    Beef,
+    Pasta,
+
 }
